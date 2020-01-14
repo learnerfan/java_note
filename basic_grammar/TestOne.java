0e4088a52659810c2b3add44b7bc6b85beba04d4
song.shifan <song.shifan@rongzer.com>
2020-01-14 22:38:24 +0800

    关键ASCII码,int的范围

--- basic_grammar/TestOne.java
+++ basic_grammar/TestOne.java
@@ -4,5 +4,35 @@ class TestOne{
         System.out.println("hello");
         FreshJuice freshJuice = new FreshJuice();
         freshJuice.test();
+        int num = 50;
+        num = num ++ * 2;
+        System.out.println(num);
+
+        int num2 = 2147483647;
+        long temp = num2 + 2L;
+        System.out.println(temp);
+        char c = 'A';
+        int num3 = 10;
+        switch(c){
+            case 'B':
+            num3 ++;
+            case 'A':
+            num3 ++;
+            case 'Y':
+            num3 ++;
+                break;
+            default:
+            num3 --;                
+        }
+        System.out.println(num3);
+
+        int num4 = 0;
+        for(int x = 0 ; x < 10 ; x++){
+            num4 += x;
+            if(x % 3 == 0){
+                break;
+            }
+        }
+        System.out.println(num4);
     }
 }
